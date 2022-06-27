0a3579d435959a750b019770b3c90b3a16e732e3
schaka39 <e1920039@oit.ac.jp>
2022-06-27 16:45:24 +0900

    URL取得した

--- src/Get.tsx
+++ src/Get.tsx
@@ -1,13 +1,33 @@
+import axios from 'axios';
+ 
+interface jsonType {
+    id:number,
+    data:string,
+    flag:boolean
+}
+ 
+export const ApiGet_Simple = (URL:string):jsonType[] => {
+ 
+    // エラー用に空データを準備
+    let return_Json:jsonType[] = [];
+ 
+    axios
+        .get<jsonType[]>('https://mocaff.net/order')
+        .then((results) => {
+            return_Json = results.data;
+            console.log(return_Json);
+      // 成功したら取得できたデータを返す
+            return return_Json;
+        })
+        .catch((error) => {
+            console.log('通信失敗');
+            console.log(error.status);
+            // 失敗したときは空のjsonを返す
+        });
+ 
+  // エラーの場合はこれを返す
+    return return_Json;
 
-import axios, { AxiosResponse } from "axios";
-
-// モックサーバーのURL　db.json
-const membersUrl = "https://mocaff.net/order";
-
-type Member = {
-  id: number;
-  title: string;
-  price: number;
+   
 };
 
- 
